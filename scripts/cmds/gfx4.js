27b9b27a3b38c1e99bedb584b77c0f753f708054
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-01 10:16:09 +0300

    ⊂(●ᆺ●)⊃

--- scripts/cmds/gfx4.js
+++ scripts/cmds/gfx4.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 module.exports = {
     config: {
         name: "gfx4",
-        author: "Samir Œ",
+        author: "keithkeizzah",
         description: "Send an image ",
         category: "gfx",
         usage: "<prompt>",
@@ -18,7 +18,7 @@ module.exports = {
         }
 
         try {
-            const apiUrl = `https://apis-samir.onrender.com/gfx4?text1=${encodeURIComponent(prompt)}&text1=${encodeURIComponent(prompt)}`;
+            const apiUrl = `https://samirxpikachuio.onrender.com/gfx4?text1=${encodeURIComponent(prompt)}&text1=${encodeURIComponent(prompt)}`;
             const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
             const imageData = Buffer.from(response.data, 'binary');
             await bot.sendPhoto(chatId, imageData);
