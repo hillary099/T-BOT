4d6cc8324429ea94fb99069bc7b5806e377b673f
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-01 11:19:34 +0300

    ⊂(●ᆺ●)⊃

--- scripts/cmds/adminonly.js
+++ scripts/cmds/adminonly.js
@@ -4,7 +4,7 @@ const config = require('../../config.json');
 module.exports = {
     config: {
         name: "adminonly",
-        author: "Samir Œ",
+        author: "keithkeizzah",
         description: "Toggle admin-only mode",
         category: "admin",
         usage: "adminonly <on|off>",
