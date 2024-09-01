82222f161232381798b0fa7787a05071c5249418
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-01 10:34:24 +0300

    ⊂(●ᆺ●)⊃

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -3,7 +3,7 @@ const config = require('../../config.json');
 module.exports = {
     config: {
         name: "prefix",
-        author: "Samir Œ",
+        author: "keithkeizzah",
         description: "bot prefix",
         category: "utility",
         usage: "prefix",
