3397bf98a38a4f0d4354db23a89baa5a569b73aa
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-09-01 11:35:34 +0300

    ⊂(●ᆺ●)⊃

--- scripts/cmds/repo.js
+++ scripts/cmds/repo.js
@@ -12,7 +12,7 @@ module.exports = {
     chatId: _0x36d3b4
   }) {
     try {
-      _0x3ab608.sendMessage(_0x36d3b4, "\n𝗞𝗲𝗶𝘁𝗵-𝗠𝗱 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗕𝗼𝘁:\n \n𝗥𝗲𝗽𝗼 𝗟𝗶𝗻𝗸: https://github.com/keithkeizzah/T-BOT\nKindly fork and Star Repo for Courage...\n Heppy Deployment!!!\n \n𝗠𝗮𝗱𝗲 𝗜𝗻 𝗞𝗲𝗻𝘆𝗮 🇰🇪𝗕𝘆 𝗞𝗲𝗶𝘁𝗵𝗸𝗲𝗶𝘇𝘇𝗮𝗵\n ");
+      _0x3ab608.sendMessage(_0x36d3b4, "\n𝗞𝗲𝗶𝘁𝗵-𝗠𝗱 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗕𝗼𝘁:\n \n𝗥𝗲𝗽𝗼 𝗟𝗶𝗻𝗸: https://github.com/keithkeizzah/T-BOT\nKindly fork and Star Repo for Courage...\n Happy Deployment!!!\n \n𝗠𝗮𝗱𝗲 𝗜𝗻 𝗞𝗲𝗻𝘆𝗮 🇰🇪𝗕𝘆 𝗞𝗲𝗶𝘁𝗵𝗸𝗲𝗶𝘇𝘇𝗮𝗵\n ");
     } catch (_0x2d2b86) {
       console.error("[ERROR]", _0x2d2b86);
       _0x3ab608.sendMessage(_0x36d3b4, "An error occurred while fetching the features.");
